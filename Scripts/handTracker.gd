f606492699ca39235d4bb7114fcebb10807f08a2
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-03-05 17:01:05 +0100

    imported the notes :)

--- Scripts/handTracker.gd
+++ Scripts/handTracker.gd
@@ -3,8 +3,8 @@ extends Node
 var json = JSON.new()
 const ORIENTATION_KEYS = ["Left", "Right"]
 var orientation = ["", ""]
-var hand1 = []	# The first hand array
-var hand2 = []	# The second hand array
+var hand1 = [] # The first hand array
+var hand2 = [] # The second hand array
 
 # Get hand data from the JSON file
 func get_data():
@@ -13,10 +13,10 @@ func get_data():
 	return content
 	
 #Use this data to copy the text-data in the landmarks into vectors and numbers to use in the game 
-func copy_to_vector(data: Dictionary): 
+func copy_to_vector(data: Dictionary):
 	return Vector2(data.x, data.y)
 	
-func base_vector(): 
+func base_vector():
 	return Vector2(0, 0)
 
 func mirror_orientation(word: String):
@@ -39,15 +39,10 @@ func _process(_delta):
 				# Get hand orientation and its data
 				orientation[0] = mirror_orientation(data_received[0].category_name)
 				hand1[index] = copy_to_vector(data_received[0].landmark_data[index])
-				if data_received.size() > 1: 
+				if data_received.size() > 1:
 					orientation[1] = mirror_orientation(data_received[1].category_name)
 					hand2[index] = copy_to_vector(data_received[1].landmark_data[index])
 				else:
 					hand2[index] = base_vector()
 		else:
 			print("Unexpected data")
-			
-	# Print the data
-	# print("Hand 1: ", orientation[0], hand1)
-	# print("Hand 2: ", orientation[1], hand2)
-		
--- Scripts/spawnCircles.gd
+++ Scripts/spawnCircles.gd
@@ -1,16 +1,33 @@
 extends Node
 
+const NOTE_MARGIN = 10 # Margin for the notes to be hit (in ms)
+
 var timeSpent = 0;
+var notes = []
+var notesArray = []
+var noteIndex = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Load the notes from the file and add the note timestamps to the array
+	var songImport = NotesImport.noteFileData
+	notes = songImport["difficulties"]["Expert"]["Single"]
 
+	notesArray = []
+	for noteKey in notes.keys():
+		notesArray.append(int(noteKey))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-
+func _process(_delta):
 	timeSpent = Time.get_ticks_msec()
-	if 
 
-	pass
+	# Print out the time spent each second
+	if timeSpent % 1000 <= NOTE_MARGIN:
+		print(timeSpent / 1000)
+
+	# print(timeSpent)
+
+	# Check if the note is in the time range
+	if notesArray[noteIndex] - timeSpent < NOTE_MARGIN:
+		print(notesArray[noteIndex], " Note hit!")
+		noteIndex += 1
